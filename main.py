4c7bb79d77b008f097913c2e38ee5344424020dc
Artemii <angap4@gmail.com>
2023-07-31 19:36:35 +1000

    Создана система переключения между экранами
    Осталось только подвязать её к уровням, хах

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
 """
 Главное пространство для запуска
 """
+import os
 import pygame as pg
+
+import utility.menu
 from utility.field import Board
-from utility.menu import start_screen
+from utility.menu import start_screen, level_screen, gameover_screen, Button
 import configparser
+
+
+def exit(*args):
+    global menu_index
+    menu_index = -1
+
+def start(*args):
+    global menu_index
+    menu_index = 1
+
+
+def load_image(name, resize_ch=1):
+    fullname = os.path.join('textures', name)
+    try:
+        image = pg.image.load(fullname)
+    except pg.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+
+    if resize_ch != 1:
+        img_size = image.get_size()
+        new_img = (img_size[0] * resize_ch, img_size[1] * resize_ch)
+        image = pg.transform.scale(image, new_img)
+    return image
+
 
 config = configparser.ConfigParser()
 config.read('config.cfg')
@@ -15,15 +43,46 @@ pg.init()
 pg.display.set_caption('Меню')
 screen = pg.display.set_mode((len_side_screen, len_side_screen))
 
-# background = Board(len_side_screen, count_cells)
-
 running = True
+menu_index = 0
+
+exit_img = pg.image.load('textures/end.png')
+img_size = exit_img.get_size()
+new_exit = (img_size[0] * 5, img_size[1] * 5)
+
+start_screen_buttons = pg.sprite.Group()
+level_screen_buttons = pg.sprite.Group()
+gameover_screen_buttons = pg.sprite.Group()
+exit_button = Button(len_side_screen // 2, len_side_screen // 2 + ((len_side_screen // 2) // 3) * 2,
+                     load_image("end.png", 7), exit,  start_screen_buttons)
+start_button = Button(len_side_screen // 2, len_side_screen // 2, load_image("start.png", 7), start,  start_screen_buttons)
+
 start_screen(screen, len_side_screen, count_cells)
+start_screen_buttons.draw(screen)
+
+list_groups_buttons = [start_screen_buttons, level_screen_buttons, gameover_screen_buttons]
+
+cur_len_snake = 0
+
 while running:
     pos = pg.mouse.get_pos()
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+        if event.type == pg.MOUSEBUTTONDOWN:
+            list_groups_buttons[menu_index].update(pg.mouse.get_pos())
+            if menu_index == -1:
+                running = False
+                break
+            screen.fill('black')
+            if menu_index == 0:
+                start_screen(screen, len_side_screen, count_cells)
+            elif menu_index == 1:
+                level_screen(screen, len_side_screen, count_cells)
+            elif menu_index == 2:
+                gameover_screen(screen, len_side_screen, count_cells, cur_len_snake)
+
+
     # background.draw(screen)
     pg.display.flip()
 
--- utility/menu.py
+++ utility/menu.py
@@ -1,46 +1,65 @@
 """
-Здесь будут содержаться коды ко всем менюшкам (см. Milestone Механики)
+Здесь будут содержаться коды ко всем меню (см. Milestone Механики)
 """
 import pygame
 from utility.field import Board
 
 
 class Button(pygame.sprite.Sprite):
-    def __int__(self, x: int, y: int, image: pygame, action, *group: pygame.sprite.Group):
+    def __init__(self, x: int, y: int, image: pygame.Surface, action, *group: pygame.sprite.Group):
         super().__init__(*group)
 
         self.action = action
 
         self.image = image
+        image_size = image.get_size()
+        x -= image_size[0] // 2
+        y -= image_size[1] // 2
         self.rect = self.image.get_rect()
 
-        self.rect.begin_coord = (x, y)
+        self.rect.topleft = (x, y)
 
-    def update(self, mouse_pos) -> None:
-
+    def update(self, mouse_pos, *args) -> int | None:
         if self.rect.collidepoint(mouse_pos):
-            self.action()
+            return self.action(args)
 
 
-def start_screen(screen, len_side_screen, count_cells):
+def background_render(screen: pygame.Surface, len_side_screen: int, count_cells: int) -> None:
+    bgrd = Board(len_side_screen, count_cells)
+    bgrd.draw(screen)
+
+
+def start_screen(screen: pygame.Surface, len_side_screen: int, count_cells: int) -> None:
     """
     Начальный экран.
 
     """
-    intro_text = ["ЗАСТАВКА", "",
-                  "Правила игры",
-                  "Если в правилах несколько строк,",
-                  "приходится выводить их построчно"]
+    background_render(screen, len_side_screen, count_cells)
 
-    bgrd = Board(len_side_screen, count_cells)
-    bgrd.draw(screen)
-    font = pygame.font.Font(None, 30)
+    intro_text = ["Snake 2.0"]
+    font = pygame.font.Font(None, 100)
     text_coord = 50
     for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('white'))
+        string_rendered = font.render(line, 1, pygame.Color('violet'))
         intro_rect = string_rendered.get_rect()
         text_coord += 10
         intro_rect.top = text_coord
-        intro_rect.x = 10
+        intro_rect.x = len_side_screen // 2 - string_rendered.get_size()[0] // 2
         text_coord += intro_rect.height
         screen.blit(string_rendered, intro_rect)
+
+
+def level_screen(screen: pygame.Surface, len_side_screen: int, count_cells: int) -> None:
+    """
+    Экран выбора уровней
+
+    :param screen:
+    :param len_side_screen:
+    :param count_cells:
+
+    """
+    background_render(screen, len_side_screen, count_cells)
+
+
+def gameover_screen(screen: pygame.Surface, len_side_screen: int, count_cells: int, len_snake: int) -> None:
+    pass
